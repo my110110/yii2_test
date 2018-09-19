5d516e91b43b15bac552524f65084a06247dd5dd
zyh110110 <446910086@qq.com>
2018-09-19 11:40:55 +0800

    1213

--- deploy.php
+++ deploy.php
@@ -17,7 +17,7 @@ add('shared_files', []);
 add('shared_dirs', []);
 
 // Writable dirs by web server 
-set('writable_dirs', ['runtime', 'web/uploads', 'web/assets']);
+set('writable_dirs', [ 'web/uploads', 'web/assets']);
 
 
 // Hosts
