263f2152aba45113fb411264111a3763baeea32d
zyh110110 <446910086@qq.com>
2018-10-24 14:45:26 +0800

    i1

---
+++ controllers/EsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+use yii\web\Controller;
+
+
+class EsController extends Controller
+{
+    public function actionIndex()
+    {
+        $user = New TestUser();
+        $user->name = '大家好';
+        $user->sex = '1';
+        $user->age = '12';
+        $user->save();
+        echo 123;
+       // return $this->render('index');
+    }
+
+}
