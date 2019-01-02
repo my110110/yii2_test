45c29eca6d9a9e969e2c584e0d6da38306708af1
zyh110110 <446910086@qq.com>
2019-01-02 17:06:05 +0800

    q1

---
+++ Jobs/OrderPingJob.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mayn
+ * Date: 2018/10/29
+ * Time: 10:42
+ */
+namespace app\Jobs;
+use app\models\TestUser;
+use Yii;
+use yii\base\BaseObject;
+use app\models\UserTest;
+
+class OrderPingJob extends BaseObject implements \yii\queue\JobInterface
+{
+    public $id;
+    public $userid;
+    public function execute($queue)
+    {
+        $id = $this->id;
+        $user = $this->userid;
+        Yii::$app->db->transaction(function () use ($id,$user)
+        {
+        if(UserTest::find()->andFilterWhere(['name'=>$user])->exists()){
+            $data = new TestUser();
+            $data->name = $user;
+            $data->age = $id;
+            $data->sex = 20;
+            $data->save();
+        }else{
+            $model = new UserTest();
+            $model->name = $user;
+            $model->type = $id;
+            $model->save();
+            if($model->save()){
+                return '成功';
+            }else{
+                return $model->getErrors();
+            }
+
+        }
+        });
+    }
+
+
+}
---
+++ deploy.php
@@ -0,0 +1,93 @@
+<?php
+namespace Deployer;
+
+require 'recipe/yii2-app-basic.php';
+
+// Project name
+set('application', 'my_project');
+
+// Project repository
+set('repository', 'git@github.com:my110110/yii2_test.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+// 保存最近五次部署，这样的话回滚最多也只能回滚到前 5 个版本
+set('keep_releases', 5);
+
+// 实践证明，这样能减少一些不必要的麻烦,如出现权限相关的问题，也可将此项设置为 true 后尝试
+set('writable_use_sudo', false);
+
+// 生产用的主机
+host('47.104.213.83')
+    ->stage('dev')
+    ->user('root')
+    ->port(22)
+    ->set('branch', 'master') // 最新的主分支部署到生产机
+    ->set('deploy_path', '/var/www/html/yii2_test')
+    ->identityFile('~/.ssh/id_rsa')
+    ->forwardAgent(true)
+    ->multiplexing(true)
+    ->set('http_user', 'www'); // 这个与 nginx 里的配置一致
+//    ->addSshOption('UserKnownHostsFile', '/dev/null')
+//    ->addSshOption('StrictHostKeyChecking', 'no');
+    
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+set('composer_options', 'install --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction');
+task('deploy:copyenv', function () {
+    run('cd {{release_path}}&& cp .env.dist .env');
+})->desc('Run copyenv');
+task('deploy:chomdweb', function () {
+    run('cd {{deploy_path}}/current/web&&chmod -R 777 *');
+})->desc('Run chomdweb');
+task('deploy:queue', function () {
+    run('cd {{deploy_path}}/current&&php yii queue/listen');
+})->desc('Run queue');
+task('deploy:run_migrations', function () {
+    run('{{bin/php}} {{release_path}}/yii migrate up --interactive=0');
+})->desc('Run migrations');
+task('deploy:clear_cache', function () {
+    run("{{bin/php}} {{release_path}}/yii cache/flush-all");
+})->desc("clear cache");
+task('nginx:restart', function () {
+    run('systemctl restart nginx');
+})->desc('Restart nginx service');
+task('php-fpm:restart', function () {
+    run('systemctl restart php-fpm');
+})->desc('Restart PHP-FPM service');
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+    'deploy:copyenv',
+    'deploy:run_migrations',
+    'deploy:symlink',
+    'deploy:chomdweb',
+    'deploy:clear_cache',
+    'cleanup',
+    'success',
+    'nginx:restart',
+    'php-fpm:restart'
+])->desc('Deploy your project');
+
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
